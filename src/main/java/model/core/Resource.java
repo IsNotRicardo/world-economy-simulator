e36ebb9e9dc6a358ab92023eb4682f0ee7fd3c17
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-11-28 12:50:43 +0200

    Make Resource class a record class

--- src/main/java/model/core/Resource.java
+++ src/main/java/model/core/Resource.java
@@ -1,37 +1,17 @@
 package model.core;
 
-public class Resource {
-    private final String name;
-    private final ResourceCategory category;
-    private final double priority;
-    private final int baseCapacity;
-    private final double productionCost;
+public record Resource (
+        String name, ResourceCategory category, double priority, int baseCapacity, double productionCost) {
 
-    public Resource(String name, ResourceCategory category, double priority, int baseCapacity, double productionCost) {
-        this.name = name;
-        this.category = category;
-        this.priority = priority;
-        this.baseCapacity = baseCapacity;
-        this.productionCost = productionCost;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPriority() {
-        return priority;
-    }
-
-    public ResourceCategory getCategory() {
-        return category;
-    }
-
-    public int getBaseCapacity() {
-        return baseCapacity;
-    }
-
-    public double getProductionCost() {
-        return productionCost;
+    public Resource {
+        if (priority < 0 || priority > 1) {
+            throw new IllegalArgumentException("Priority must be between 0 and 1.");
+        }
+        if (baseCapacity < 0) {
+            throw new IllegalArgumentException("Base capacity cannot be negative.");
+        }
+        if (productionCost < 0) {
+            throw new IllegalArgumentException("Production cost cannot be negative.");
+        }
     }
 }
