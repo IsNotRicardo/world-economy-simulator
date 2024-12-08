4dff81259d158ae2f50133e61540793b4f08c2fb
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-08 17:48:25 +0200

    remove redundant text field methods and update the fx:id names.

--- src/main/java/controller/InitialController.java
+++ src/main/java/controller/InitialController.java
@@ -1,84 +1,74 @@
 package controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import view.InitialView;
-
+import javafx.scene.input.MouseEvent;
 
 public class InitialController {
-    @FXML
-    public TextField simTimeField;
-    @FXML
-    public TextField delayField;
-    @FXML
-    public Button minusDelayButton;
-    @FXML
-    public Button resetDelayButton;
-    @FXML
-    public Button plusDelayButton;
-    @FXML
-    public TextField timeElapsedField;
-    @FXML
-    public Button addCountryButton;
-    public TextField countryNameField;
-    public TextField populationField;
-    public TextField moneyField;
-    public Button saveCountryButton;
-    public TextField moenyField;
-    public ProgressBar Progress;
-    public TextField ResourceNameField;
-    public TextField ResourceCategoryField;
-    public Button saveResource;
-    public TextField ResourceBaseCapacity;
-    public TextField ResourceProductionCost;
-    public Slider ResourcePriority;
-    public TextField CountryName;
-    public TextField CountryPopulation;
-    public TextField CountryMoney;
-    public Button saveCountry1;
-    public TextField CountryStarterResources;
-    public TextField CountryOwnedResources;
 
-    public void decreaseDelay(){
+	@FXML
+	private TextField simTimeField;
+	@FXML
+	private TextField delayField;
+	@FXML
+	private Button minusDelayButton;
+	@FXML
+	private Button resetDelayButton;
+	@FXML
+	private Button plusDelayButton;
+	@FXML
+	private ProgressBar Progress;
+	@FXML
+	private TextField ResourceNameField;
+	@FXML
+	private TextField ResourceCategoryField;
+	@FXML
+	private TextField baseCapacityField;
+	@FXML
+	private TextField productionCostField;
+	@FXML
+	private Slider prioritySlider;
+	@FXML
+	private Button saveResource;
+	@FXML
+	private TextField countryNameField;
+	@FXML
+	private TextField populationField;
+	@FXML
+	private TextField moneyField;
+	@FXML
+	private Button addCountryField;
+	@FXML
+	private TextField starterResourcesField;
+	@FXML
+	private TextField ownedResourcesField;
 
-    }
-    public void resetDelay(){
+	@FXML
+	public void decreaseDelay(ActionEvent actionEvent) {
 
-    }
-    public void IncrementDelay(){
+	}
 
-    }
-    public void saveCountry(){}
+	@FXML
+	public void resetDelay(ActionEvent actionEvent) {
+	}
 
-    public void getMoney(ActionEvent actionEvent) {
-    }
+	@FXML
+	public void IncrementDelay(ActionEvent actionEvent) {
+	}
 
-    public void getPopulation(ActionEvent actionEvent) {
-    }
+	@FXML
+	public void setPriority(MouseEvent mouseEvent) {
+	}
 
-    public void getCountryName(ActionEvent actionEvent) {
-    }
+	@FXML
+	public void addResource(ActionEvent actionEvent) {
+	}
 
-    public void openCountryCreation(ActionEvent actionEvent) {
-
-    }
-
-    public void getMoeny(ActionEvent actionEvent) {
-    }
-    public void getDelay(){
-
-    }
-    public void updateSimulationTime(){
-        
-    }
+	@FXML
+	public void addCountry(ActionEvent actionEvent) {
+	}
 }
-
