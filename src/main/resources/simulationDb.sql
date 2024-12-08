69b3955ef4b10c00680de1a2f17d805be1a6fba9
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-12-08 19:45:20 +0200

    Modify simulationDb.sql, add simulationMetrics.sql and create file for user permissions

--- src/main/resources/simulationDb.sql
+++ src/main/resources/simulationDb.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS simulation;
-
 USE simulation;
 
+-- Drop and recreate default tables
 DROP TABLE IF EXISTS default_resource;
 DROP TABLE IF EXISTS default_country;
+DROP TABLE IF EXISTS default_country_resource;
 
-CREATE TABLE IF NOT EXISTS default_resource
+CREATE TABLE default_resource
 (
     id              INT PRIMARY KEY AUTO_INCREMENT,
     name            VARCHAR(255) NOT NULL UNIQUE,
@@ -39,13 +40,7 @@ VALUES ('Wood', 'NATURAL', 0.8, 10, 10.0),
        ('Sugar', 'AGRICULTURAL', 0.9, 10, 20.0),
        ('Tobacco', 'AGRICULTURAL', 0.1, 10, 30.0);
 
-CREATE TABLE IF NOT EXISTS resource LIKE default_resource;
-
-INSERT IGNORE INTO resource
-SELECT *
-FROM default_resource;
-
-CREATE TABLE IF NOT EXISTS default_country
+CREATE TABLE default_country
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
     name       VARCHAR(255) NOT NULL UNIQUE,
@@ -60,12 +55,65 @@ VALUES ('Finland', 276600000000.00, 5527573),
        ('Denmark', 404200000000.00, 5831404),
        ('Iceland', 31020000000.00, 388425);
 
+CREATE TABLE default_country_resource
+(
+    country_id  INT,
+    resource_id INT,
+    PRIMARY KEY (country_id, resource_id),
+    FOREIGN KEY (country_id) REFERENCES default_country (id) ON DELETE CASCADE,
+    FOREIGN KEY (resource_id) REFERENCES default_resource (id) ON DELETE CASCADE
+);
+
+INSERT IGNORE INTO default_country_resource (country_id, resource_id)
+VALUES
+-- Finland
+((SELECT id FROM default_country WHERE name = 'Finland'), (SELECT id FROM default_resource WHERE name = 'Food')),
+((SELECT id FROM default_country WHERE name = 'Finland'), (SELECT id FROM default_resource WHERE name = 'Water')),
+((SELECT id FROM default_country WHERE name = 'Finland'), (SELECT id FROM default_resource WHERE name = 'Wood')),
+((SELECT id FROM default_country WHERE name = 'Finland'), (SELECT id FROM default_resource WHERE name = 'Iron')),
+((SELECT id FROM default_country WHERE name = 'Finland'), (SELECT id FROM default_resource WHERE name = 'Copper')),
+-- Sweden
+((SELECT id FROM default_country WHERE name = 'Sweden'), (SELECT id FROM default_resource WHERE name = 'Food')),
+((SELECT id FROM default_country WHERE name = 'Sweden'), (SELECT id FROM default_resource WHERE name = 'Water')),
+((SELECT id FROM default_country WHERE name = 'Sweden'), (SELECT id FROM default_resource WHERE name = 'Wood')),
+((SELECT id FROM default_country WHERE name = 'Sweden'), (SELECT id FROM default_resource WHERE name = 'Iron')),
+((SELECT id FROM default_country WHERE name = 'Sweden'), (SELECT id FROM default_resource WHERE name = 'Gold')),
+-- Norway
+((SELECT id FROM default_country WHERE name = 'Norway'), (SELECT id FROM default_resource WHERE name = 'Food')),
+((SELECT id FROM default_country WHERE name = 'Norway'), (SELECT id FROM default_resource WHERE name = 'Water')),
+((SELECT id FROM default_country WHERE name = 'Norway'), (SELECT id FROM default_resource WHERE name = 'Oil')),
+((SELECT id FROM default_country WHERE name = 'Norway'), (SELECT id FROM default_resource WHERE name = 'Coal')),
+((SELECT id FROM default_country WHERE name = 'Norway'), (SELECT id FROM default_resource WHERE name = 'Aluminium')),
+-- Denmark
+((SELECT id FROM default_country WHERE name = 'Denmark'), (SELECT id FROM default_resource WHERE name = 'Food')),
+((SELECT id FROM default_country WHERE name = 'Denmark'), (SELECT id FROM default_resource WHERE name = 'Water')),
+((SELECT id FROM default_country WHERE name = 'Denmark'), (SELECT id FROM default_resource WHERE name = 'Wood')),
+((SELECT id FROM default_country WHERE name = 'Denmark'), (SELECT id FROM default_resource WHERE name = 'Iron')),
+((SELECT id FROM default_country WHERE name = 'Denmark'), (SELECT id FROM default_resource WHERE name = 'Coal')),
+-- Iceland
+((SELECT id FROM default_country WHERE name = 'Iceland'), (SELECT id FROM default_resource WHERE name = 'Food')),
+((SELECT id FROM default_country WHERE name = 'Iceland'), (SELECT id FROM default_resource WHERE name = 'Water')),
+((SELECT id FROM default_country WHERE name = 'Iceland'), (SELECT id FROM default_resource WHERE name = 'Wood')),
+((SELECT id FROM default_country WHERE name = 'Iceland'), (SELECT id FROM default_resource WHERE name = 'Iron')),
+((SELECT id FROM default_country WHERE name = 'Iceland'), (SELECT id FROM default_resource WHERE name = 'Uranium'));
+
+-- Create resource table if it doesn't exist and populate it if empty
+CREATE TABLE IF NOT EXISTS resource LIKE default_resource;
+
+INSERT IGNORE INTO resource
+SELECT *
+FROM default_resource
+WHERE (SELECT COUNT(*) FROM resource) = 0;
+
+-- Create country table if it doesn't exist and populate it if empty
 CREATE TABLE IF NOT EXISTS country LIKE default_country;
 
 INSERT IGNORE INTO country
 SELECT *
-FROM default_country;
+FROM default_country
+WHERE (SELECT COUNT(*) FROM country) = 0;
 
+-- Create many-to-many relationship table if it doesn't exist
 CREATE TABLE IF NOT EXISTS country_resource
 (
     country_id  INT,
@@ -75,65 +123,15 @@ CREATE TABLE IF NOT EXISTS country_resource
     FOREIGN KEY (resource_id) REFERENCES resource (id) ON DELETE CASCADE
 );
 
+-- Populate many-to-many relationship table only if country and resource tables match default tables and if it's empty
 INSERT IGNORE INTO country_resource (country_id, resource_id)
-VALUES
--- Finland
-((SELECT id FROM country WHERE name = 'Finland'), (SELECT id FROM resource WHERE name = 'Food')),
-((SELECT id FROM country WHERE name = 'Finland'), (SELECT id FROM resource WHERE name = 'Water')),
-((SELECT id FROM country WHERE name = 'Finland'), (SELECT id FROM resource WHERE name = 'Wood')),
-((SELECT id FROM country WHERE name = 'Finland'), (SELECT id FROM resource WHERE name = 'Iron')),
-((SELECT id FROM country WHERE name = 'Finland'), (SELECT id FROM resource WHERE name = 'Copper')),
--- Sweden
-((SELECT id FROM country WHERE name = 'Sweden'), (SELECT id FROM resource WHERE name = 'Food')),
-((SELECT id FROM country WHERE name = 'Sweden'), (SELECT id FROM resource WHERE name = 'Water')),
-((SELECT id FROM country WHERE name = 'Sweden'), (SELECT id FROM resource WHERE name = 'Wood')),
-((SELECT id FROM country WHERE name = 'Sweden'), (SELECT id FROM resource WHERE name = 'Iron')),
-((SELECT id FROM country WHERE name = 'Sweden'), (SELECT id FROM resource WHERE name = 'Gold')),
--- Norway
-((SELECT id FROM country WHERE name = 'Norway'), (SELECT id FROM resource WHERE name = 'Food')),
-((SELECT id FROM country WHERE name = 'Norway'), (SELECT id FROM resource WHERE name = 'Water')),
-((SELECT id FROM country WHERE name = 'Norway'), (SELECT id FROM resource WHERE name = 'Oil')),
-((SELECT id FROM country WHERE name = 'Norway'), (SELECT id FROM resource WHERE name = 'Coal')),
-((SELECT id FROM country WHERE name = 'Norway'), (SELECT id FROM resource WHERE name = 'Aluminium')),
--- Denmark
-((SELECT id FROM country WHERE name = 'Denmark'), (SELECT id FROM resource WHERE name = 'Food')),
-((SELECT id FROM country WHERE name = 'Denmark'), (SELECT id FROM resource WHERE name = 'Water')),
-((SELECT id FROM country WHERE name = 'Denmark'), (SELECT id FROM resource WHERE name = 'Wood')),
-((SELECT id FROM country WHERE name = 'Denmark'), (SELECT id FROM resource WHERE name = 'Iron')),
-((SELECT id FROM country WHERE name = 'Denmark'), (SELECT id FROM resource WHERE name = 'Coal')),
--- Iceland
-((SELECT id FROM country WHERE name = 'Iceland'), (SELECT id FROM resource WHERE name = 'Food')),
-((SELECT id FROM country WHERE name = 'Iceland'), (SELECT id FROM resource WHERE name = 'Water')),
-((SELECT id FROM country WHERE name = 'Iceland'), (SELECT id FROM resource WHERE name = 'Wood')),
-((SELECT id FROM country WHERE name = 'Iceland'), (SELECT id FROM resource WHERE name = 'Iron')),
-((SELECT id FROM country WHERE name = 'Iceland'), (SELECT id FROM resource WHERE name = 'Uranium'));
-
-/*
-CREATE TABLE IF NOT EXISTS averages
-(
-    country_id  INT,
-    resource_id INT,
-    average     DOUBLE,
-    PRIMARY KEY (country_id, resource_id),
-    FOREIGN KEY (country_id) REFERENCES country (id) ON DELETE CASCADE,
-    FOREIGN KEY (resource_id) REFERENCES resource (id) ON DELETE CASCADE
-);
-*/
-/*
-SELECT *
-FROM default_resource;
-
-SELECT *
-FROM resource;
-
-SELECT *
-FROM default_country;
-
-SELECT *
-FROM country;
-
-SELECT country_id, c.name AS country_name, r.name AS resource_name
-FROM country_resource cr
-         JOIN country c ON cr.country_id = c.id
-         JOIN resource r ON cr.resource_id = r.id;
- */
+SELECT dc.id, dr.id
+FROM default_country_resource dcr
+         JOIN default_country dc ON dcr.country_id = dc.id
+         JOIN default_resource dr ON dcr.resource_id = dr.id
+WHERE NOT EXISTS (SELECT 1
+                  FROM country c
+                           JOIN resource r ON c.id = r.id
+                  WHERE c.name != dc.name
+                     OR r.name != dr.name)
+  AND (SELECT COUNT(*) FROM country_resource) = 0;
