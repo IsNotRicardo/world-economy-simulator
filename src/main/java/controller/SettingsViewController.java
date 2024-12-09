7106b502338661fe48ee97739a83558e6bd17d20
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-12-09 15:37:50 +0200

    Add simulation settings to layout with default values

--- src/main/java/controller/SettingsViewController.java
+++ src/main/java/controller/SettingsViewController.java
@@ -2,13 +2,29 @@ package controller;
 
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import model.simulation.SimulationConfig;
 
 public class SettingsViewController {
-    private static final int TAB_OFFSET_VALUE = 55;
+    private static final int TAB_OFFSET_VALUE = 68;
+    private static final int TAB_HEIGHT = 48;
 
     @FXML
     private TabPane tabPane;
+    @FXML
+    private VBox simulationSettingsVBox;
+    @FXML
+    private TextField simulationTimeField;
+    @FXML
+    private TextField simulationDelayField;
+    @FXML
+    private TextField countrySupplySampleField;
+    @FXML
+    private TextField populationSegmentSizeField;
 
     @FXML
     public void initialize() {
@@ -20,5 +36,19 @@ public class SettingsViewController {
                 () -> (tabPane.getWidth() - TAB_OFFSET_VALUE) / tabPane.getTabs().size(),
                 tabPane.widthProperty(), tabPane.getTabs()
         ));
+        tabPane.tabMinHeightProperty().set(TAB_HEIGHT);
+        tabPane.tabMaxHeightProperty().set(TAB_HEIGHT);
+
+        Insets margin = new Insets(30, 30, 0, 30);
+        simulationSettingsVBox.getChildren().forEach(child -> {
+            if (child instanceof HBox) {
+                VBox.setMargin(child, margin);
+            }
+        });
+
+        simulationTimeField.setText(Integer.toString(SimulationConfig.getSimulationTime()));
+        simulationDelayField.setText(Integer.toString(SimulationConfig.getSimulationDelay()));
+        countrySupplySampleField.setText(Integer.toString(SimulationConfig.getSupplyArchiveTime()));
+        populationSegmentSizeField.setText(Integer.toString(SimulationConfig.getPopulationSegmentSize()));
     }
 }
