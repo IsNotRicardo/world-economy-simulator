b2e4df862a94247ce753c1f21a0162aa873a812e
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-09 22:09:28 +0200

    add method for finding a resource by name. add checks for existing resources to check if the values stored in the database are the same of that of the given resource.

--- src/main/java/dao/ResourceDao.java
+++ src/main/java/dao/ResourceDao.java
@@ -4,7 +4,6 @@ import entity.Resource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceException;
-import org.hibernate.exception.SQLGrammarException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,26 +15,58 @@ public class ResourceDao {
 		EntityManager em = datasource.MariaDbConnection.getEntityManager();
 		em.getTransaction().begin();
 		try {
-			Resource existingResource = em.createQuery("SELECT r FROM Resource r WHERE r.name = :name", Resource.class)
-			                              .setParameter("name", resource.getName())
-			                              .getSingleResult();
-			existingResource.setPriority(resource.getPriority());
-			existingResource.setBaseCapacity(resource.getBaseCapacity());
-			existingResource.setProductionCost(resource.getProductionCost());
-			em.merge(existingResource);
-			logger.debug("Updated existing resource: {}", resource.getName());
-		} catch (NoResultException e) {
-			em.persist(resource);
-			logger.debug("Persisted new resource: {}", resource.getName());
-		} catch (SQLGrammarException e) {
-			em.getTransaction().rollback();
-			logger.error("SQL grammar error persisting resource: {}", resource.getName(), e);
-			throw new PersistenceException("SQL grammar error", e);
+			Resource existingResource = findByName(resource.getName());
+			if (existingResource != null) {
+				if (existingResource.getPriority() != resource.getPriority()) {
+					existingResource.setPriority(resource.getPriority());
+				}
+				if (existingResource.getBaseCapacity() != resource.getBaseCapacity()) {
+					existingResource.setBaseCapacity(resource.getBaseCapacity());
+				}
+				if (existingResource.getProductionCost() != resource.getProductionCost()) {
+					existingResource.setProductionCost(resource.getProductionCost());
+				}
+				em.merge(existingResource);
+				logger.debug("Updated existing resource: {}", resource.getName());
+			} else {
+				em.persist(resource);
+				logger.debug("Persisted new resource: {}", resource.getName());
+			}
+			em.getTransaction().commit();
 		} catch (Exception e) {
 			em.getTransaction().rollback();
 			logger.error("Error persisting resource: {}", resource.getName(), e);
 			throw e;
 		}
-		em.getTransaction().commit();
+	}
+
+	public Resource findByName(String name) {
+		EntityManager em = datasource.MariaDbConnection.getEntityManager();
+		try {
+			return em.createQuery("SELECT r FROM Resource r WHERE r.name = :name", Resource.class)
+			         .setParameter("name", name)
+			         .getSingleResult();
+		} catch (NoResultException e) {
+			logger.debug("Resource not found: {}", name);
+			return null;
+		} catch (Exception e) {
+			logger.error("Error finding resource by name: {}", name, e);
+			throw e;
+		}
+	}
+
+	public void deleteByName(String name) {
+		EntityManager em = datasource.MariaDbConnection.getEntityManager();
+		try {
+			em.createQuery("DELETE FROM Resource r WHERE r.name = :name")
+			  .setParameter("name", name)
+			  .executeUpdate();
+			em.getTransaction().commit();
+			logger.debug("Deleted resource by name: {}", name);
+			} catch (PersistenceException e) {
+			em.getTransaction().rollback();
+			logger.error("Error deleting resource by name: {}", name, e);
+			throw e;
+		}
 	}
 }
