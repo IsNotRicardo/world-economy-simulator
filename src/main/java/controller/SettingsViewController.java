9b53efefa65489ead4099c8415bd6754cd1d1bdc
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-12-09 12:03:38 +0200

    Add styling to settings layout and tab logic to controller

--- src/main/java/controller/SettingsViewController.java
+++ src/main/java/controller/SettingsViewController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
 public class SettingsViewController {
+    private static final int TAB_OFFSET_VALUE = 55;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    public void initialize() {
+        tabPane.tabMinWidthProperty().bind(Bindings.createDoubleBinding(
+                () -> (tabPane.getWidth() - TAB_OFFSET_VALUE) / tabPane.getTabs().size(),
+                tabPane.widthProperty(), tabPane.getTabs()
+        ));
+        tabPane.tabMaxWidthProperty().bind(Bindings.createDoubleBinding(
+                () -> (tabPane.getWidth() - TAB_OFFSET_VALUE) / tabPane.getTabs().size(),
+                tabPane.widthProperty(), tabPane.getTabs()
+        ));
+    }
 }
