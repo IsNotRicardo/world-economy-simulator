b34f333072c03c38b675aff4f1ad048da66ffb0f
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-09 22:16:00 +0200

    add script for dropping the simulation_user if exists and after creating the same user and granting them privileges to manipulate the simulation database.

--- src/main/resources/simulationUser.sql
+++ src/main/resources/simulationUser.sql
@@ -1,3 +1,6 @@
-CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON simulation.* TO 'appuser'@'localhost';
+DROP USER IF EXISTS 'simulation_user'@'localhost';
+
+CREATE USER IF NOT EXISTS 'simulation_user'@'localhost' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON simulation.* TO 'simulation_user'@'localhost';
 FLUSH PRIVILEGES;
