a3943596fdd7f27d9f325a1b19bb477f1d38cb47
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-09 21:55:37 +0200

    add quantity variable to country resource and also setters and getters for all variables.

--- src/main/java/entity/CountryResource.java
+++ src/main/java/entity/CountryResource.java
@@ -4,32 +4,67 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "country_resource")
-@IdClass(CountryResourceId.class)
 public class CountryResource {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
 	@ManyToOne
-	@JoinColumn(name = "country_id", referencedColumnName = "id")
+	@JoinColumn(name = "country_id", nullable = false)
 	private Country country;
 
-	@Id
 	@ManyToOne
-	@JoinColumn(name = "resource_id", referencedColumnName = "id")
+	@JoinColumn(name = "resource_id", nullable = false)
 	private Resource resource;
 
-	public void setCountry(Country country) {
-		this.country = country;
+	@Column(name = "quantity", nullable = false)
+	private int quantity;
+
+	public CountryResource() {
 	}
 
-	public void setResource(Resource resource) {
+	public CountryResource(Country country, Resource resource, int quantity) {
+		this.country = country;
 		this.resource = resource;
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public Country getCountry() {
 		return country;
 	}
 
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
 	public Resource getResource() {
 		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "CountryResource [id=" + id + ", country=" + country + ", resource=" + resource + ", quantity=" + quantity
+				+ "]";
 	}
 }
