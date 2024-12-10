49218aa87cb9ddc6b653dbc4b5560b7179b209bd
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-10 18:34:35 +0200

    feat: integrate DAO layer for simulation metrics storage
    - Added DAOs for saving country, resource, and resource node metrics.
    - Replaced simulated metric saving with database persistence functionality.
    - Implemented logic to handle absent database entities and create them if needed.

--- src/main/java/model/simulation/Simulator.java
+++ src/main/java/model/simulation/Simulator.java
@@ -1,173 +1,245 @@
 package model.simulation;
 
+import dao.*;
+import entity.*;
 import model.core.*;
 
 import java.util.List;
 import java.util.Map;
 
 public class Simulator {
-    private final Clock clock = Clock.getInstance();
-    private final EventList eventList = new EventList();
+	private final Clock clock = Clock.getInstance();
+	private final EventList eventList = new EventList();
 
-    private final List<Resource> resources;
-    private final List<Country> countries;
+	private final List<Resource> resources;
+	private final List<Country> countries;
 
-    public Simulator(List<Resource> resources, List<Country> countries) {
-        this.resources = resources;
-        this.countries = countries;
+	private final ResourceDao resourceDao = new ResourceDao();
+	private final CountryDao countryDao = new CountryDao();
+	private final CountryMetricsDao countryMetricsDao = new CountryMetricsDao();
+	private final ResourceMetricsDao resourceMetricsDao = new ResourceMetricsDao();
+	private final ResourceNodeMetricsDao resourceNodeMetricsDao = new ResourceNodeMetricsDao();
 
-        for (Country country : countries) {
-            country.addAllCountries(countries);
-        }
-    }
+	public Simulator(List<Resource> resources, List<Country> countries) {
+		this.resources = resources;
+		this.countries = countries;
 
-    public void runSimulation() {
-        initializeSimulation();
+		for (Country country : countries) {
+			country.addAllCountries(countries);
+		}
+	}
 
-        while (clock.getTime() < SimulationConfig.getSimulationTime()) {
-            if (!clock.isPaused()) {
-                // A-phase
-                // Advance the clock to the next event time
-                Event nextEvent = eventList.peekNextEvent();
-                if (nextEvent != null) {
-                    clock.setTime(nextEvent.getTime());
-                }
+	public void runSimulation() {
+		initializeSimulation();
 
-                // B-phase
-                // Process all events that are scheduled to occur at the current time
-                while (nextEvent != null && nextEvent.getTime() == clock.getTime()) {
-                    processEvent(eventList.getNextEvent());
-                    nextEvent = eventList.peekNextEvent();
-                }
+		while (clock.getTime() < SimulationConfig.getSimulationTime()) {
+			if (!clock.isPaused()) {
+				// A-phase
+				// Advance the clock to the next event time
+				Event nextEvent = eventList.peekNextEvent();
+				if (nextEvent != null) {
+					clock.setTime(nextEvent.getTime());
+				}
 
-                // C-phase
-                // Create new events based on specific conditions
-                // Currently, there are no available conditions to check
+				// B-phase
+				// Process all events that are scheduled to occur at the current time
+				while (nextEvent != null && nextEvent.getTime() == clock.getTime()) {
+					processEvent(eventList.getNextEvent());
+					nextEvent = eventList.peekNextEvent();
+				}
 
-                saveMetrics();
-                System.out.println("\nDay " + clock.getTime() + " completed.");
-            }
-            try {
-                Thread.sleep(SimulationConfig.getSimulationDelay());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.out.println("Simulation interrupted.");
-            }
-        }
+				// C-phase
+				// Create new events based on specific conditions
+				// Currently, there are no available conditions to check
 
-        finalizeSimulation();
-    }
+				saveMetricsToDb();
+				System.out.println("\nDay " + clock.getTime() + " completed.");
+			}
+			try {
+				Thread.sleep(SimulationConfig.getSimulationDelay());
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				System.out.println("Simulation interrupted.");
+			}
+		}
 
-    private void initializeSimulation() {
-        // Since all current events happen daily, adding +1 to the current time is sufficient
-        int nextEventTime = clock.getTime() + 1;
+		finalizeSimulation();
+	}
 
-        eventList.addEvent(new Event(EventType.UPDATE_PEOPLE, nextEventTime));
-        eventList.addEvent(new Event(EventType.OBTAIN_RESOURCES, nextEventTime));
-        eventList.addEvent(new Event(EventType.SERVE_PEOPLE, nextEventTime));
-        eventList.addEvent(new Event(EventType.REQUEST_RESOURCES, nextEventTime));
+	private void initializeSimulation() {
+		// Since all current events happen daily, adding +1 to the current time is sufficient
+		int nextEventTime = clock.getTime() + 1;
 
-        System.out.println("Simulation initialized.");
-    }
+		eventList.addEvent(new Event(EventType.UPDATE_PEOPLE, nextEventTime));
+		eventList.addEvent(new Event(EventType.OBTAIN_RESOURCES, nextEventTime));
+		eventList.addEvent(new Event(EventType.SERVE_PEOPLE, nextEventTime));
+		eventList.addEvent(new Event(EventType.REQUEST_RESOURCES, nextEventTime));
 
-    private void processEvent(Event event) {
-        int nextEventTime = clock.getTime() + 1;
+		System.out.println("Simulation initialized.");
+	}
 
-        switch (event.getType()) {
-            case UPDATE_PEOPLE:
-                for (Country country : countries) {
-                    country.updatePeople();
-                }
-                eventList.addEvent(new Event(EventType.UPDATE_PEOPLE, nextEventTime));
-                break;
-            case OBTAIN_RESOURCES:
-                for (Country country : countries) {
-                    country.obtainResources();
-                }
-                eventList.addEvent(new Event(EventType.OBTAIN_RESOURCES, nextEventTime));
-                break;
-            case SERVE_PEOPLE:
-                for (Country country : countries) {
-                    country.servePeople();
-                }
-                eventList.addEvent(new Event(EventType.SERVE_PEOPLE, nextEventTime));
-                break;
-            case REQUEST_RESOURCES:
-                for (Country country : countries) {
-                    country.requestResources();
-                }
-                eventList.addEvent(new Event(EventType.REQUEST_RESOURCES, nextEventTime));
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown event type: " + event.getType());
-        }
-    }
+	private void processEvent(Event event) {
+		int nextEventTime = clock.getTime() + 1;
 
-    private void saveMetrics() {
-        for (Country country : countries) {
-            // Save the country metrics
-            // Call the DAO here
-            System.out.println("\nSaving metrics for " + country.getName());
-            System.out.println("Population: " + country.getPopulation());
-            System.out.println("Money: " + country.getMoney());
+		switch (event.getType()) {
+			case UPDATE_PEOPLE:
+				for (Country country : countries) {
+					country.updatePeople();
+				}
+				eventList.addEvent(new Event(EventType.UPDATE_PEOPLE, nextEventTime));
+				break;
+			case OBTAIN_RESOURCES:
+				for (Country country : countries) {
+					country.obtainResources();
+				}
+				eventList.addEvent(new Event(EventType.OBTAIN_RESOURCES, nextEventTime));
+				break;
+			case SERVE_PEOPLE:
+				for (Country country : countries) {
+					country.servePeople();
+				}
+				eventList.addEvent(new Event(EventType.SERVE_PEOPLE, nextEventTime));
+				break;
+			case REQUEST_RESOURCES:
+				for (Country country : countries) {
+					country.requestResources();
+				}
+				eventList.addEvent(new Event(EventType.REQUEST_RESOURCES, nextEventTime));
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown event type: " + event.getType());
+		}
+	}
 
-//            // Save the resource metrics per country
-//            for (Map.Entry<Resource, ResourceInfo> entry : country.getResourceStorage().entrySet()) {
-//                // Call the DAO here
-//                Resource resource = entry.getKey();
-//                ResourceInfo resourceInfo = entry.getValue();
-//                System.out.println("\nResource: " + resource.name());
-//                System.out.println("--- Quantity: " + resourceInfo.getQuantity());
-//                System.out.println("--- Value: " + resourceInfo.getValue());
-//            }
-//
-//            // Save the resource node metrics per country
-//            for (ResourceNode resourceNode : country.getResourceNodes()) {
-//                // Call the DAO here
-//                System.out.println("\nResource Node: " + resourceNode.getResource().name());
-//                System.out.println("--- Stored Resources: " + resourceNode.getStoredResources());
-//                System.out.println("--- Base Capacity: " + resourceNode.getBaseCapacity());
-//                System.out.println("--- Tier: " + resourceNode.getTier());
-//            }
+	private void saveMetricsToDb() {
 
-//            for (Person person : country.getPeopleObjects()) {
-//                System.out.println("Happiness: " + person.getHappiness());
-//                System.out.println("Preferences: " + person.getPreferences());
-//                System.out.println("Demand: " + person.getDemand());
-//            }
-        }
-    }
+		for (Country country : countries) {
+			System.out.println("\nSaving metrics for " + country.getName());
 
-    private void finalizeSimulation() {
-        // Generate a report or summary of the simulation results
-        generateReport();
+			CountryEntity countryEntity = countryDao.findByName(country.getName());
+			if (countryEntity == null) {
+				countryEntity = new CountryEntity(country.getName(), country.getMoney(), country.getPopulation());
+				countryDao.persist(countryEntity);
+			}
 
-        System.out.println("\nSimulation finalized.");
-    }
+			double individualBudget = country.getSegmentBudget() / SimulationConfig.getPopulationSegmentSize();
 
-    private void generateReport() {
-        // Implement logic to generate a report or summary of the simulation results
-        System.out.println("\nGenerating simulation report...");
-        // Example: Print summary statistics
-        for (Country country : countries) {
-            System.out.println("Country: " + country.getName());
-            System.out.println("Population: " + country.getPopulation());
-            System.out.println("Money: " + country.getMoney());
+			CountryMetricsEntity countryMetrics =
+					new CountryMetricsEntity(clock.getTime(), countryEntity, country.getPopulation(),
+					                         country.getMoney(), country.getAverageHappiness(), individualBudget);
 
-            for (Map.Entry<Resource, ResourceInfo> entry : country.getResourceStorage().entrySet()) {
-                Resource resource = entry.getKey();
-                ResourceInfo resourceInfo = entry.getValue();
-                System.out.println("Resource: " + resource.name());
-                System.out.println("--- Quantity: " + resourceInfo.getQuantity());
-                System.out.println("--- Value: " + resourceInfo.getValue());
-            }
+			try {
+				countryMetricsDao.persist(countryMetrics);
+			} catch (Exception e) {
+				System.out.println("Error persisting country metrics: " + e.getMessage());
+			}
 
-            for (ResourceNode resourceNode : country.getResourceNodes()) {
-                System.out.println("Resource Node: " + resourceNode.getResource().name());
-                System.out.println("--- Stored Resources: " + resourceNode.getStoredResources());
-                System.out.println("--- Base Capacity: " + resourceNode.getBaseCapacity());
-                System.out.println("--- Tier: " + resourceNode.getTier());
-            }
-        }
-    }
+			if (country.getResourceStorage().isEmpty() && country.getResourceNodes().isEmpty()) {
+				System.out.println("No resources or resource nodes to save metrics for.");
+				continue;
+			}
+
+			for (Map.Entry<Resource, ResourceInfo> entry : country.getResourceStorage().entrySet()) {
+				Resource resource = entry.getKey();
+				ResourceInfo resourceInfo = entry.getValue();
+
+				ResourceEntity resourceEntity = getResourceEntity(resource, null);
+
+				ResourceMetricsEntity resourceMetrics =
+						new ResourceMetricsEntity(clock.getTime(), countryEntity, resourceEntity,
+						                          resourceInfo.getQuantity(), resourceInfo.getValue());
+				try {
+					resourceMetricsDao.persist(resourceMetrics);
+				} catch (Exception e) {
+					System.out.println("Error persisting resource metrics: " + e.getMessage());
+				}
+			}
+
+			if (country.getResourceNodes().isEmpty()) {
+				System.out.println("No resource nodes to save metrics for.");
+				continue;
+			}
+
+			for (ResourceNode resourceNode : country.getResourceNodes()) {
+
+				ResourceEntity resourceEntity = getResourceEntity(null, resourceNode);
+
+				ResourceNodeMetricsEntity resourceNodeMetrics =
+						new ResourceNodeMetricsEntity(clock.getTime(), countryEntity, resourceEntity,
+						                              resourceNode.getMaxCapacity(), resourceNode.getProductionCost(),
+						                              resourceNode.getTier());
+
+				try {
+					resourceNodeMetricsDao.persist(resourceNodeMetrics);
+				} catch (Exception e) {
+					System.out.println("Error persisting resource node metrics: " + e.getMessage());
+				}
+			}
+		}
+	}
+
+	private void finalizeSimulation() {
+		// Generate a report or summary of the simulation results
+		generateReport();
+
+		System.out.println("\nSimulation finalized.");
+	}
+
+	private void generateReport() {
+		// Implement logic to generate a report or summary of the simulation results
+		System.out.println("\nGenerating simulation report...");
+		// Example: Print summary statistics
+		for (Country country : countries) {
+			System.out.println("Country: " + country.getName());
+			System.out.println("Population: " + country.getPopulation());
+			System.out.println("Money: " + country.getMoney());
+
+			for (Map.Entry<Resource, ResourceInfo> entry : country.getResourceStorage().entrySet()) {
+				Resource resource = entry.getKey();
+				ResourceInfo resourceInfo = entry.getValue();
+				System.out.println("Resource: " + resource.name());
+				System.out.println("--- Quantity: " + resourceInfo.getQuantity());
+				System.out.println("--- Value: " + resourceInfo.getValue());
+			}
+
+			for (ResourceNode resourceNode : country.getResourceNodes()) {
+				System.out.println("Resource Node: " + resourceNode.getResource().name());
+				System.out.println("--- Stored Resources: " + resourceNode.getStoredResources());
+				System.out.println("--- Base Capacity: " + resourceNode.getBaseCapacity());
+				System.out.println("--- Tier: " + resourceNode.getTier());
+			}
+		}
+	}
+
+	private ResourceEntity getResourceEntity(Resource resource, ResourceNode resourceNode) {
+		String resourceName;
+		double priority;
+		int baseCapacity;
+		double productionCost;
+
+		if (resource != null) {
+			resourceName = resource.name();
+			priority = resource.priority();
+			baseCapacity = resource.baseCapacity();
+			productionCost = resource.productionCost();
+		} else if (resourceNode != null) {
+			resourceName = resourceNode.getResource().name();
+			priority = resourceNode.getResource().priority();
+			baseCapacity = resourceNode.getResource().baseCapacity();
+			productionCost = resourceNode.getResource().productionCost();
+		} else {
+			throw new IllegalArgumentException("Resource or ResourceNode must be provided.");
+		}
+
+		ResourceEntity resourceEntity = resourceDao.findByName(resourceName);
+		if (resourceEntity == null) {
+			resourceEntity = new ResourceEntity(resourceName, priority, baseCapacity, productionCost);
+			try {
+				resourceDao.persist(resourceEntity);
+			} catch (Exception e) {
+				System.out.println("Error persisting resource: " + e.getMessage());
+			}
+		}
+		return resourceEntity;
+	}
 }
