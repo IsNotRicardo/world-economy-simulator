5229afa4597a5d538e3c6df240c77575c5e32788
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-12-10 15:25:20 +0200

    Add countries tab and logic in layout

--- src/main/java/controller/SettingsViewController.java
+++ src/main/java/controller/SettingsViewController.java
@@ -6,10 +6,12 @@ import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import model.core.Country;
 import model.core.Resource;
 import model.simulation.SimulationConfig;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +20,13 @@ public class SettingsViewController {
     private static final int TAB_HEIGHT = 48;
 
     private List<Resource> resourceList = new ArrayList<>();
+    private List<Country> countryList = new ArrayList<>();
+
     private boolean isResourceEditingMode;
     private int currentResourceIndex;
+
+    private boolean isCountryEditingMode;
+    private int currentCountryIndex;
 
     @FXML
     private TabPane tabPane;
@@ -71,6 +78,32 @@ public class SettingsViewController {
     private Region resourceOptionalRegion;
     @FXML
     private Button deleteResourceButton;
+    @FXML
+    private ListView<String> countryListView;
+    @FXML
+    private VBox countrySettingsVBox;
+    @FXML
+    private Label countryItemsLabel;
+    @FXML
+    private TextField countryNameField;
+    @FXML
+    private TextField countryInitialMoneyField;
+    @FXML
+    private TextField countryInitialPopulationField;
+    @FXML
+    private Label countryNameErrorLabel;
+    @FXML
+    private Label countryInitialMoneyErrorLabel;
+    @FXML
+    private Label countryInitialPopulationErrorLabel;
+    @FXML
+    private Button addCountryButton;
+    @FXML
+    private Button saveCountryButton;
+    @FXML
+    private Region countryOptionalRegion;
+    @FXML
+    private Button deleteCountryButton;
 
     @FXML
     public void initialize() {
@@ -115,6 +148,7 @@ public class SettingsViewController {
         });
 
         Insets titleMargin = new Insets(0, 30, 0, 30);
+
         resourceSettingsVBox.getChildren().forEach(child -> {
             if (child instanceof Label) {
                 VBox.setMargin(child, titleMargin);
@@ -123,6 +157,15 @@ public class SettingsViewController {
             }
         });
         changeResourceButtonVisibility(false);
+
+        countrySettingsVBox.getChildren().forEach(child -> {
+            if (child instanceof Label) {
+                VBox.setMargin(child, titleMargin);
+            } else {
+                VBox.setMargin(child, margin);
+            }
+        });
+        changeCountryButtonVisibility(false);
     }
 
     private void validateGeneralField(TextField textField, Label errorLabel, int defaultValue, int minValue, String errorMessage) {
@@ -183,7 +226,7 @@ public class SettingsViewController {
         }
     }
 
-    private boolean isResourceValid() {
+    private boolean validateResourceFields() {
         boolean isNameValid = validateStringField(resourceNameField, resourceNameErrorLabel,
                 "Name must not be empty");
         boolean isPriorityValid = validateDoubleField(resourcePriorityField, resourcePriorityErrorLabel,
@@ -194,6 +237,14 @@ public class SettingsViewController {
                 0.0, Double.MAX_VALUE, "Production cost cannot be negative");
 
         return isNameValid && isPriorityValid && isBaseCapacityValid && isProductionCostValid;
+    }
+
+    private boolean validateCountryFields() {
+        boolean isNameValid = validateStringField(countryNameField, countryNameErrorLabel, "Name must not be empty");
+        boolean isInitialMoneyValid = validateDoubleField(countryInitialMoneyField, countryInitialMoneyErrorLabel, 0.0, Double.MAX_VALUE, "Initial money cannot be negative");
+        boolean isInitialPopulationValid = validateIntField(countryInitialPopulationField, countryInitialPopulationErrorLabel, 1, "Initial population must be positive");
+
+        return isNameValid && isInitialMoneyValid && isInitialPopulationValid;
     }
 
     @FXML
@@ -217,7 +268,7 @@ public class SettingsViewController {
 
     @FXML
     public void addResource() {
-        if (isResourceValid()) {
+        if (validateResourceFields()) {
             String name = resourceNameField.getText();
             double priority = Double.parseDouble(resourcePriorityField.getText());
             int baseCapacity = Integer.parseInt(resourceBaseCapacityField.getText());
@@ -262,7 +313,7 @@ public class SettingsViewController {
     public void saveResource() {
         int selectedIndex = resourceListView.getSelectionModel().getSelectedIndex();
         if (selectedIndex != -1) {
-            if (isResourceValid()) {
+            if (validateResourceFields()) {
                 String name = resourceNameField.getText();
                 double priority = Double.parseDouble(resourcePriorityField.getText());
                 int baseCapacity = Integer.parseInt(resourceBaseCapacityField.getText());
@@ -294,6 +345,102 @@ public class SettingsViewController {
 
                 clearResourceFields();
                 changeResourceButtonVisibility(false);
+            }
+        }
+    }
+
+    @FXML
+    public void newCountry() {
+        if (resolveIsCountryNotSaved()) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("New Country");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to create a new country?\nAny unsaved changes will be lost.");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isEmpty() || result.get() != ButtonType.OK) {
+                return;
+            }
+        }
+        clearCountryFields();
+        changeCountryButtonVisibility(false);
+        countryListView.getSelectionModel().clearSelection();
+    }
+
+    @FXML
+    public void addCountry() {
+        if (validateCountryFields()) {
+            String name = countryNameField.getText();
+            double initialMoney = Double.parseDouble(countryInitialMoneyField.getText());
+            int initialPopulation = Integer.parseInt(countryInitialPopulationField.getText());
+
+            Country newCountry = new Country(name, initialMoney, initialPopulation, new HashMap<>(), new HashMap<>());
+            countryList.add(newCountry);
+            countryListView.getItems().add(name);
+            clearCountryFields();
+        }
+    }
+
+    @FXML
+    public void handleCountrySelection() {
+        if (resolveIsCountryNotSaved()) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Select Country");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to change countries?\nAny unsaved changes will be lost.");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isEmpty() || result.get() != ButtonType.OK) {
+                countryListView.getSelectionModel().select(currentCountryIndex);
+                return;
+            }
+        }
+        int selectedIndex = countryListView.getSelectionModel().getSelectedIndex();
+
+        if (selectedIndex != -1) {
+            Country selectedCountry = countryList.get(selectedIndex);
+            countryNameField.setText(selectedCountry.getName());
+            countryInitialMoneyField.setText(String.valueOf(selectedCountry.getMoney()));
+            countryInitialPopulationField.setText(String.valueOf(selectedCountry.getPopulation()));
+            changeCountryButtonVisibility(true);
+            currentCountryIndex = selectedIndex;
+        }
+    }
+
+    @FXML
+    public void saveCountry() {
+        int selectedIndex = countryListView.getSelectionModel().getSelectedIndex();
+        if (selectedIndex != -1) {
+            if (validateCountryFields()) {
+                String name = countryNameField.getText();
+                double initialMoney = Double.parseDouble(countryInitialMoneyField.getText());
+                int initialPopulation = Integer.parseInt(countryInitialPopulationField.getText());
+
+                Country updatedCountry = new Country(name, initialMoney, initialPopulation, new HashMap<>(), new HashMap<>());
+                countryList.set(selectedIndex, updatedCountry);
+                countryListView.getItems().set(selectedIndex, name);
+                countryListView.getSelectionModel().select(selectedIndex);
+            }
+        }
+    }
+
+    @FXML
+    public void deleteCountry() {
+        int selectedIndex = countryListView.getSelectionModel().getSelectedIndex();
+        if (selectedIndex != -1) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete Country");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to delete this country?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                countryList.remove(selectedIndex);
+                countryListView.getItems().remove(selectedIndex);
+                countryListView.getSelectionModel().clearSelection();
+
+                clearCountryFields();
+                changeCountryButtonVisibility(false);
             }
         }
     }
@@ -346,5 +493,52 @@ public class SettingsViewController {
         resourcePriorityErrorLabel.setText("");
         resourceBaseCapacityErrorLabel.setText("");
         resourceProductionCostErrorLabel.setText("");
+    }
+
+    private void changeCountryButtonVisibility(boolean editingMode) {
+        isCountryEditingMode = editingMode;
+
+        if (editingMode) {
+            countryItemsLabel.setText("Modify Country");
+        } else {
+            countryItemsLabel.setText("Insert Country");
+        }
+
+        addCountryButton.setVisible(!editingMode);
+        addCountryButton.setManaged(!editingMode);
+
+        saveCountryButton.setVisible(editingMode);
+        saveCountryButton.setManaged(editingMode);
+
+        countryOptionalRegion.setVisible(editingMode);
+        countryOptionalRegion.setManaged(editingMode);
+
+        deleteCountryButton.setVisible(editingMode);
+        deleteCountryButton.setManaged(editingMode);
+    }
+
+    private boolean resolveIsCountryNotSaved() {
+        if (isCountryEditingMode) {
+            Country savedCountry = countryList.get(currentCountryIndex);
+            String savedName = savedCountry.getName();
+            String savedInitialMoney = String.valueOf(savedCountry.getMoney());
+            String savedInitialPopulation = String.valueOf(savedCountry.getPopulation());
+
+            return !countryNameField.getText().equals(savedName) || !countryInitialMoneyField.getText().equals(savedInitialMoney) ||
+                    !countryInitialPopulationField.getText().equals(savedInitialPopulation);
+        } else {
+            return !countryNameField.getText().isEmpty() || !countryInitialMoneyField.getText().isEmpty() ||
+                    !countryInitialPopulationField.getText().isEmpty();
+        }
+    }
+
+    private void clearCountryFields() {
+        countryNameField.setText("");
+        countryInitialMoneyField.setText("");
+        countryInitialPopulationField.setText("");
+
+        countryNameErrorLabel.setText("");
+        countryInitialMoneyErrorLabel.setText("");
+        countryInitialPopulationErrorLabel.setText("");
     }
 }
