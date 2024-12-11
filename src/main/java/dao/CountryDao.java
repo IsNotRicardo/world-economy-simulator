367f3aa257f9d3b6f68df35fbeb299c3fe0a4c96
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-11 22:53:57 +0200

    refactor: simplify CountryDao persistence logic
    - Replaced conditional logic with a single `merge` operation for both insert and update scenarios.
    - Added `EntityManager` cleanup in `finally` blocks to ensure resource management.
    - Removed unused `Objects` import.
    - Deleted unused CRUD methods (delete, deleteByName, deleteAll, findCountryIdByName) for simplification.

--- src/main/java/dao/CountryDao.java
+++ src/main/java/dao/CountryDao.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Objects;
 
 public class CountryDao {
 
@@ -17,25 +16,17 @@ public class CountryDao {
 		EntityManager em = datasource.MariaDbConnection.getEntityManager();
 		em.getTransaction().begin();
 		try {
-			CountryEntity existingCountry = findByName(country.getName());
-			if (existingCountry != null && Objects.equals(existingCountry.getName(), country.getName())) {
-				if (existingCountry.getMoney() != country.getMoney()) {
-					existingCountry.setMoney(country.getMoney());
-				}
-				if (existingCountry.getPopulation() != country.getPopulation()) {
-					existingCountry.setPopulation(country.getPopulation());
-				}
-				em.merge(existingCountry);
-				logger.debug("Updated existing country: {}", country.getName());
-			} else {
-				em.persist(country);
-				logger.debug("Persisted new country: {}", country.getName());
-			}
+			// Use merge for both update and insert scenarios
+			em.merge(country);
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			em.getTransaction().rollback();
 			logger.error("Error persisting country: {}", country.getName(), e);
 			throw e;
+		} finally {
+			if (em.isOpen()) {
+				em.close();
+			}
 		}
 	}
 
@@ -50,6 +41,10 @@ public class CountryDao {
 		} catch (Exception e) {
 			logger.error("Error finding country by name: {}", name, e);
 			throw e;
+		} finally {
+			if (em.isOpen()) {
+				em.close();
+			}
 		}
 	}
 
@@ -60,66 +55,10 @@ public class CountryDao {
 		} catch (Exception e) {
 			logger.error("Error finding all countries", e);
 			throw e;
-		}
-	}
-
-	public long findCountryIdByName(String countryName) {
-		EntityManager em = datasource.MariaDbConnection.getEntityManager();
-		try {
-			return em.createQuery("SELECT c.id FROM CountryEntity c WHERE c.name = :name", Long.class)
-			         .setParameter("name", countryName).getSingleResult();
-		} catch (NoResultException e) {
-			logger.debug("Country not found: {}", countryName);
-			return 0;
-		} catch (Exception e) {
-			logger.error("Error finding country by name: {}", countryName, e);
-			throw e;
-		}
-	}
-
-	public void delete(CountryEntity country) {
-		EntityManager em = datasource.MariaDbConnection.getEntityManager();
-		em.getTransaction().begin();
-		try {
-			if (!em.contains(country)) {
-				country = em.merge(country);
+		} finally {
+			if (em.isOpen()) {
+				em.close();
 			}
-			em.remove(country);
-			em.getTransaction().commit();
-			logger.debug("Deleted country: {}", country.getName());
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-			logger.error("Error deleting country: {}", country.getName(), e);
-			throw e;
-		}
-	}
-
-	public void deleteByName(String name) {
-		EntityManager em = datasource.MariaDbConnection.getEntityManager();
-		em.getTransaction().begin();
-		try {
-			em.createQuery("DELETE FROM CountryEntity c WHERE c.name = :name").setParameter("name", name)
-			  .executeUpdate();
-			em.getTransaction().commit();
-			logger.debug("Deleted country by name: {}", name);
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-			logger.error("Error deleting country by name: {}", name, e);
-			throw e;
-		}
-	}
-
-	public void deleteAll() {
-		EntityManager em = datasource.MariaDbConnection.getEntityManager();
-		em.getTransaction().begin();
-		try {
-			em.createQuery("DELETE FROM CountryEntity").executeUpdate();
-			em.getTransaction().commit();
-			logger.debug("Deleted all countries");
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-			logger.error("Error deleting all countries", e);
-			throw e;
 		}
 	}
 }
