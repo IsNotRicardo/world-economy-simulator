538100f6ed2e46434a2b4add425e5003dcc4aeaf
Saku Heinonen <sakuhei@metropolia.fi>
2024-12-11 16:52:13 +0200

    feat(sql): grant all privileges to simulation_user
    - Updated `simulationUser.sql` to grant all privileges instead of specific ones.
    - Ensures the user has comprehensive access to the simulation database.

--- src/main/resources/scripts/simulationUser.sql
+++ src/main/resources/scripts/simulationUser.sql
@@ -2,5 +2,5 @@ DROP USER IF EXISTS 'simulation_user'@'localhost';
 
 CREATE USER IF NOT EXISTS 'simulation_user'@'localhost' IDENTIFIED BY 'password';
 
-GRANT CREATE, SELECT, UPDATE, DELETE ON simulation.* TO 'simulation_user'@'localhost';
+GRANT ALL PRIVILEGES ON simulation.* TO 'simulation_user'@'localhost';
 FLUSH PRIVILEGES;
