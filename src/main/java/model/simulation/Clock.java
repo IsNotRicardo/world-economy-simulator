033b95f7939add70af7b3e630733d1b1c48ab682
ItsNotRicardo <142903523+IsNotRicardo@users.noreply.github.com>
2024-12-14 18:57:26 +0200

    Add JavaDoc for the Clock class

--- src/main/java/model/simulation/Clock.java
+++ src/main/java/model/simulation/Clock.java
@@ -1,16 +1,27 @@
 package model.simulation;
 
+/**
+ * Clock represents a singleton clock that keeps track of the simulation time.
+ * It manages the current time and the paused state of the simulation.
+ */
 public class Clock {
     private static Clock instance;
     private int time;
     private boolean paused;
 
+    /**
+     * Constructs a new Clock.
+     * This constructor is private to enforce the singleton pattern.
+     */
     private Clock() {
         this.time = 0;
         this.paused = false;
     }
 
-    // Singleton instance
+    /**
+     * Gets the singleton instance of the Clock.
+     * @return the singleton instance
+     */
     public static Clock getInstance() {
         if (instance == null) {
             instance = new Clock();
@@ -18,27 +29,40 @@ public class Clock {
         return instance;
     }
 
-    // Get the current time
+    /**
+     * Gets the current time of the simulation.
+     * @return the current time
+     */
     public int getTime() {
         return time;
     }
 
-    // Set the current time
+    /**
+     * Sets the current time of the simulation.
+     * @param time the time to set
+     */
     public void setTime(int time) {
         this.time = time;
     }
 
-    // Check if the clock is paused
+    /**
+     * Checks if the clock is paused.
+     * @return true if the clock is paused, false otherwise
+     */
     public boolean isPaused() {
         return paused;
     }
 
-    // Pause the clock
+    /**
+     * Pauses the clock.
+     */
     public void pause() {
         paused = true;
     }
 
-    // Resume the clock
+    /**
+     * Resumes the clock.
+     */
     public void resume() {
         paused = false;
     }
